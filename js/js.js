7c98445a557ab7e5e6b3b6d7e5b9fe949cc0553a
Sasha <vinar262@gmail.com>
2016-07-03 07:08:05 +0000

    test

---
+++ js/js.js
@@ -0,0 +1,96 @@
+/*for(var i = 1;i < 7 ;i++){
+    $("#floorback1"+ String(i)).fadeOut;
+}*/
+
+$("#floor11").click(function() {
+    fadeall();
+    $("#floorback11").fadeIn();
+});
+
+$("#floor12").click(function() {
+    fadeall();
+    $("#floorback12").fadeIn();
+});
+$("#floor13").click(function() {
+    fadeall();
+    $("#floorback13").fadeIn();
+});
+$("#floor14").click(function() {
+    fadeall();
+    $("#floorback14").fadeIn();
+});
+$("#floor15").click(function() {
+    fadeall();
+    $("#floorback15").fadeIn();
+});
+$("#floor16").click(function() {
+    fadeall();
+    $("#floorback16").fadeIn();
+});
+$("#floor21").click(function() {
+    fadeall();
+    $("#floorback21").fadeIn();
+});
+$("#floor22").click(function() {
+    fadeall();
+    $("#floorback22").fadeIn();
+});
+$("#floor23").click(function() {
+    fadeall();
+    $("#floorback23").fadeIn();
+});
+$("#floor24").click(function() {
+    fadeall();
+    $("#floorback24").fadeIn();
+});
+$("#floor25").click(function() {
+    fadeall();
+    $("#floorback25").fadeIn();
+});
+$("#floor31").click(function() {
+    fadeall();
+    $("#floorback31").fadeIn();
+});
+$("#floor32").click(function() {
+    fadeall();
+    $("#floorback32").fadeIn();
+});
+$("#floor33").click(function() {
+    fadeall();
+    $("#floorback33").fadeIn();
+});
+$("#floor34").click(function() {
+    fadeall();
+    $("#floorback34").fadeIn();
+});
+$("#floor35").click(function() {
+    fadeall();
+    $("#floorback35").fadeIn();
+});
+$("#floor36").click(function() {
+    fadeall();
+    $("#floorback36").fadeIn();
+});
+
+
+function fadeall() {
+    $("#floorback11").fadeOut();
+    $("#floorback12").fadeOut();
+    $("#floorback13").fadeOut();
+    $("#floorback14").fadeOut();
+    $("#floorback15").fadeOut();
+    $("#floorback16").fadeOut();
+    $("#floorback21").fadeOut();
+    $("#floorback22").fadeOut();
+    $("#floorback23").fadeOut();
+    $("#floorback24").fadeOut();
+    $("#floorback25").fadeOut();
+    $("#floorback31").fadeOut();
+    $("#floorback32").fadeOut();
+    $("#floorback33").fadeOut();
+    $("#floorback34").fadeOut();
+    $("#floorback35").fadeOut();
+    $("#floorback36").fadeOut();
+}
+fadeall()
+$("#floorback11").fadeIn();
